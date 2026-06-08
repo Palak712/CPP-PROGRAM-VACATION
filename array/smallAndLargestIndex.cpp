39e7bbcd3e6154d2c17ee42b8863da696e9a14d3
Palak712 <palakbaranwal05@gmail.com>
2026-06-08 09:47:22 +0530

    auto-commit

--- array/smallAndLargestIndex.cpp
+++ array/smallAndLargestIndex.cpp
@@ -4,12 +4,19 @@ int main(){
     int arr[6]={321,324,545,223,345,653};
     int smallest=INT_MAX;
     int largest=INT_MIN;
+    int sIndex,lIndex;
     int size=sizeof(arr)/sizeof(arr[0]);
     for(int i=0;i<size;i++){
-        smallest=min(smallest,arr[i]);
-        largest=max(largest,arr[i]);
+        if(arr[i]<smallest){
+            smallest=arr[i];
+            sIndex=i;
+        }
+        if(arr[i]>largest){
+            largest=arr[i];
+            lIndex=i;
+        }
     }
-    cout<<"smallest number is "<<smallest<<endl;
-    cout<<"largest number is "<<largest<<endl;
+    cout<<"Smallest number is "<<smallest << " at index " << sIndex << endl;
+    cout<<"Largest number is "<<largest<< " at index " << lIndex << endl;
     return 0;
 }
