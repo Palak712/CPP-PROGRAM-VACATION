233db3edabb8ecb60851cc5eee13d66a9242311b
Palak712 <palakbaranwal05@gmail.com>
2026-06-10 09:44:01 +0530

    auto-commit

--- array/UniqueNums.cpp
+++ array/UniqueNums.cpp
@@ -6,7 +6,7 @@ int main(){
     for(int i=0;i<size;i++){
         int count=0;
         for(int j=0;j<size;j++){
-            if(arr[i]=arr[j])
+            if(arr[i]==arr[j])
                 count++;
         }
         if(count==1)
