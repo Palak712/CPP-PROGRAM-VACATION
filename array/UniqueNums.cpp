ebf7ca4cf004447357e9057dd844fec110cbe0f4
Palak712 <palakbaranwal05@gmail.com>
2026-06-10 10:11:34 +0530

    auto-commit

--- array/UniqueNums.cpp
+++ array/UniqueNums.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int arr[]={2,4,5,3,5,4,1,8};
+    int arr[]={2,4,5,3,5,5,4,1,8};
     int size=sizeof(arr)/sizeof(arr[0]);
     for(int i=0;i<size;i++){
         int count=0;
